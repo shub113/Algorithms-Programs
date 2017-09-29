f9f64b289d6f11079b77ff61348fc0e45870731c
Shubham Thakur <shubfb@gmail.com>
2017-09-29 09:22:02 +0530

    Add files via upload

--- programs/BubbleSort.java
+++ programs/BubbleSort.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
 
 import com.bridgeit.AlgorithmPrograms.utility.UtilitySearchSort;
 
-public class BubbleSort {
+public class BubbleSort<T> {
 	public static void main(String[]args){
 		Scanner scan= new Scanner(System.in);
 		UtilitySearchSort utility= new UtilitySearchSort();
 		
 		System.out.println("Enter number of input");
 		int input=scan.nextInt();
-		int[] s= new int[input];
-		System.out.println("Enter "+input+" Integer");
-		for(int i=0;i<input;i++){
-			s[i]=scan.nextInt();
-		}
-		System.out.println("\nEntered Integer : ");
+		System.out.println("Choose options");
+		System.out.println("1) Integer");
+		System.out.println("2) String");
+		int choice= scan.nextInt();
+		System.out.println("Enter "+input+" inputs");
+		Integer[] integer= new Integer[input];
+		String[] stringg= new String[input];
+		if(choice==1){
+			for(int i=0;i<input;i++){
+				integer[i]=scan.nextInt();
+			}utility.bubbleSort(integer);
+		}else if(choice==2){
+			for(int i=0;i<input;i++){
+				stringg[i]=scan.next();
+			}utility.bubbleSort(stringg);
+		}else
+			System.out.println("Invalid choice");
+		/*System.out.println("\nEntered input : ");
 		for(int i:s){
 			System.out.println(i);
-		}
-		s=utility.bubbleSort(s);
-		System.out.println("\nSorted Integer : ");
-		for(int i:s){
-			System.out.println(i);
-		}
+		}*/
+		
 	}
 }
--- programs/InsertionSort.java
+++ programs/InsertionSort.java
@@ -10,19 +10,22 @@ public class InsertionSort {
 		UtilitySearchSort utility= new UtilitySearchSort();
 		System.out.println("Enter number of input");
 		int input=scan.nextInt();
-		String[] s= new String[input];
-		System.out.println("Enter "+input+" strings");
-		for(int i=0;i<input;i++){
-			s[i]=scan.next();
-		}
-		System.out.println("\nEntered String : ");
-		for(String i:s){
-			System.out.println(i);
-		}
-		s=utility.insertionSort(s);
-		System.out.println("\nSorted String : ");
-		for(String i:s){
-			System.out.println(i);
-		}
+		System.out.println("Choose options");
+		System.out.println("1) Integer");
+		System.out.println("2) String");
+		int choice= scan.nextInt();
+		System.out.println("Enter "+input+" inputs");
+		Integer[] integer= new Integer[input];
+		String[] stringg= new String[input];
+		if(choice==1){
+			for(int i=0;i<input;i++){
+				integer[i]=scan.nextInt();
+			}utility.insertionsort(integer);
+		}else if(choice==2){
+			for(int i=0;i<input;i++){
+				stringg[i]=scan.next();
+			}utility.insertionsort(stringg);
+		}else
+			System.out.println("Invalid choice");
 	}
 }
